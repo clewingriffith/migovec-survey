d6d7b0ddfb9cb7a25a2c168ad2258740ae56d172
Clewin Griffith <clewingriffith@gmail.com>
2013-03-17 10:55:37 +0000

    ready to push

--- migovec-survey.py
+++ migovec-survey.py
@@ -1,4 +1,5 @@
 import webapp2
+from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
@@ -8,6 +9,22 @@ class MainPage(webapp2.RequestHandler):
       
       #self.response.write(file.read())
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+class Login(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		if user:
+			self.response.headers['Content-Type'] = 'text/plain'
+			if users.is_current_user_admin():
+				self.response.out.write('Hello admin user, ' + user.nickname())
+			else:
+				self.response.out.write('Hello normal user, ' + user.nickname())
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
+class GetMarkers(webapp2.RequestHandler):
+	def get(self):
+		pass
+
+app = webapp2.WSGIApplication([('/', MainPage), ('/login', Login)],
                               debug=True)
 

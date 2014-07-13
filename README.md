ca4a51e9378d46b62955d5c9c8d489c07a6ddb80
Clewin Griffith <clewin@loki.Home>
2014-07-13 09:47:22 +0100

    Use python multiprocessing module to scale and cut map tiles in parallel. Eats a lot more memory though.

--- README.md
+++ README.md
@@ -3,3 +3,42 @@ migovec-survey
 
 The aim of this site is to provide a more interactive version of the cave survey for the caves of Migovec
 
+How it works
+----------------
+
+The migovec survey site uses the Google Maps Javascript API.   For the normal
+'paper' view it takes a high resolution image of the drawn survey and scales it
+to various zoom levels, and then cuts up the images into lots of smaller tiles.
+This allows your browser to load only the bits of the image that it needs to 
+draw what you are looking at.   On top of the image tiles it displays labels 
+suitable for the level of detail you are looking at.
+
+Building
+----------
+
+Most of the code is javascript and python.   The only real build step needed is
+to scale and cut the survey into tiles.  
+
+    ant build 
+
+will start a python script (createTiles.py) which makes calls to imageMagick convert.
+This step is very CPU and memory intensive. It'll split the job over all the CPUs cores
+you have.   
+
+
+Deploying
+------------
+
+Run 
+    ant dist
+to copy the files into a dist folder ready for deployment.    
+
+You'll need the google-appengine pythonn download to actually deploy
+
+    /path/to/google_appengine/appcfg.py update dist/migovec-survey
+    
+ should deploy to the interweb. The version field in src/app.yaml will control 
+ where the deployed version goes.  version: 4 would be deployed to 
+ http://4.migovec-survey.appspot.com/
+ 
+ 
--- createTiles.py
+++ createTiles.py
@@ -4,6 +4,7 @@ import os
 import fnmatch
 import re
 import subprocess
+import multiprocessing
 
 def getParametersFromFileName(filename):
 	print "Examining filename ",filename
@@ -57,16 +58,21 @@ def createScaledImage(root,  filename, sourceParameters, destinationParameters):
 	os.path.join("build", relRoot, "it_%(zoomLevel)d_%(imageSize)d_%(xTile)d_%(yTile)d.png" % destinationParameters)],
 	shell=True)
  	
+def scaleJob(args):
+	root,  filename, sourceParameters, destinationParameters = args
+	createScaledImage(root,  filename, sourceParameters, destinationParameters)
 
-def processSource(root,filename):
+def processSource((root,filename)):
+	print "ProcessSource",root,filename
 	sourceParameters = getParametersFromFileName(filename)
 	numLevelsToCreate = calcNumLevelsToCreate(sourceParameters['imageSize'])
 	print "Creating ",numLevelsToCreate, " zoom levels"
+
 	for incZoomLevel in range(0,numLevelsToCreate):
 		destParams = getDestinationParameters(sourceParameters, incZoomLevel)
 		print destParams
 		createScaledImage(root,  filename, sourceParameters, destParams)
-		
+
 def cutIntoTiles(root, filename):
 	sourceParameters = getParametersFromFileName(filename)
 	print sourceParameters
@@ -92,11 +98,17 @@ def cutIntoTiles(root, filename):
 			print "oldfileName=",filename+".%d"%idx
 			os.rename(os.path.join(root,oldFileName), os.path.join(root,newFileName))
 
+def cutJob(args):
+	root, filename = args
+	cutIntoTiles(root, filename)
+
 if __name__ == '__main__':
 
 	print "Generating Google maps tiles"
 	print "============================"
 
+	print "Step 1: Scale source files to different resolutions"
+	print "==================================================="
 	
 	source_files = []
 	for root, dirnames, filenames in os.walk('src/survey'):
@@ -105,15 +117,16 @@ if __name__ == '__main__':
 
 	print "Found source files: ", source_files
 
-	for root,filename in source_files:
-		processSource(root,filename)
+	pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+	pool.map(processSource,  source_files)
 	
-	print "Cutting intermediate files into tiles"
+	print "Step 2: Cutting intermediate files into tiles"
+	print "============================================="
 	intermediate_files = []
 	for root, dirnames, filenames in os.walk('build/survey'):
 	  for filename in fnmatch.filter(filenames, 'it_*.png'):
 	      intermediate_files.append((root, filename))
 	print intermediate_files;
-	for root,filename in intermediate_files:
-		cutIntoTiles(root,filename)
+
+	pool.map(cutJob, intermediate_files)
 	print "Done."
